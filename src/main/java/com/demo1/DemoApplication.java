8696d23c01ce6850074ded4f60fc0fffb8d10f64
Momo <Momo@hbwl.com>
2023-01-02 20:12:14 +0800

    third commit

--- src/main/java/com/demo1/DemoApplication.java
+++ src/main/java/com/demo1/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("aaa...");
 		System.out.println("bbb...");
+		System.out.println("master...");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
